a0e1027401d94683c7d13026cfb88c60cbfa79ee
xm137 <35855821+1375272391@users.noreply.github.com>
2024-02-22 15:26:05 +0800

    Update test.cpp

--- shim/ui/test.cpp
+++ shim/ui/test.cpp
@@ -1,9 +1,1 @@
-#include <stdint.h>
-
-namespace android {
-    extern "C" void _ZN7android13GraphicBufferC2Ejjij(uint32_t inWidth, uint32_t inHeight, int inFormat, uint32_t inUsage);
-
-    extern "C" void _ZN7android13GraphicBufferC2Ev(uint32_t inWidth, uint32_t inWidth, int inFormat, uint32_t inUsage) {
-        _ZN7android13GraphicBufferC2Ejjij(inWidth, inHeight, inFormat, inUsage);
-    }
-}
+extern "C" void _ZN7android13GraphicBufferC2Ejjij() {}
